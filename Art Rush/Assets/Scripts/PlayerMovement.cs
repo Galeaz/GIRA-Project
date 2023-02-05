cb20eb3de6f50c044d280a3dd8f7b3f8a5f534f7
Wongaboy <ajwong1001@gmail.com>
2023-02-04 18:32:51 -0800

    Fixed Movement Controller

--- Art Rush/Assets/Scripts/PlayerMovement.cs
+++ Art Rush/Assets/Scripts/PlayerMovement.cs
@@ -4,33 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 3.0f;
-    public float rotationSpeed;
-
     public CharacterController controller;
-    private float horizontalInput;
-    private float verticalInput;
+    public float playerSpeed = 5.0f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized;
 
-        Vector3 direction = new Vector3(horizontalInput, 0f, verticalInput);
-
-        if (direction != Vector3.zero)
+        if (move.magnitude >= 0.1f)
         {
-            transform.rotation = Quaternion.LookRotation(direction);
-      
+            float target_angle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target_angle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            controller.Move(move * Time.deltaTime * playerSpeed);
         }
-        controller.Move(direction * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("E was pressed");
+        }
     }
 }
