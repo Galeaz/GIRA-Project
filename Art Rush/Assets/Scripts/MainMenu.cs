c07944fb21b9b7ec97e23f1f6c452cbfc91a1160
Galeaz <50804911+Galeaz@users.noreply.github.com>
2023-02-05 17:16:21 -0800

    Started coding menu
    Added functionality to buttons, still figuring out restart and continue to show in UI while paused game

--- Art Rush/Assets/Scripts/MainMenu.cs
+++ Art Rush/Assets/Scripts/MainMenu.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+
+    //singleton
+    public static MainMenu instance;
+
+    public GameObject singleplayer_Button;
+    public GameObject multiplayer_Button;
+    public GameObject restartButton;
+    public GameObject continue_Button;
+
+    private void Awake()
+    {
+        instance = this;
+        singleplayer_Button.SetActive(true); //shows start button
+        multiplayer_Button.SetActive(true); //shows start button
+        restartButton.SetActive(false); //hides restart button
+        continue_Button.SetActive(false); //hides exit button
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.SetActive(true); //activates menu
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlaySingleGame()
     {
-        
+        SceneManager.LoadScene("Singleplayer Game"); //loads game scene
+    }
+    /*
+    public void PlayMultiplayerGame()
+    {
+        SceneManager.LoadScene("Multiplayer Game"); //loads game scene
+        //function that calls for second player(?) ---------------------------------------- in progress
+    }
+    */
+    public void QuitGame()
+    {
+        Application.Quit(); //closes game
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("Settings Menu"); //opens secondary menu for settings
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits"); //opens secondary menu for team credits
     }
 }
