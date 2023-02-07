1697bab8978def8afe6457761e43a605410c58c6
Wongaboy <ajwong1001@gmail.com>
2023-02-06 20:02:46 -0800

    Tidy Up Code and In progress Interactables

--- Art Rush/Assets/Scripts/PlayerCollisions.cs
+++ Art Rush/Assets/Scripts/PlayerCollisions.cs
@@ -2,22 +2,19 @@ using UnityEngine;
 
 public class PlayerCollisions : MonoBehaviour
 {
-    [SerializeReference]
-    private PlayerMovement movement;
+    // [SerializeReference]
+    // private PlayerMovement movement;
 
     void OnCollisionEnter (Collision collisionInfo)
     {     
         if (collisionInfo.collider.tag == "Counter")
         {
-            // (collisionInfo.
             Debug.Log("We are touching a Counter");
-            // movement.touching = collisionInfo.;
         }
 
         if (collisionInfo.collider.tag == "Item Spawner")
         {
             Debug.Log("We are touching a Item Spawner");
-            // movement.touching = collisionInfo.gameObject;
         }
     }
 }
--- Art Rush/Assets/Scripts/PlayerMovement.cs
+++ Art Rush/Assets/Scripts/PlayerMovement.cs
@@ -6,56 +6,52 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Reference to the Character Controller
     [SerializeReference]
     private CharacterController controller;
 
-    [SerializeField]
-    private float normalSpeed = 15.0f;
+    // Player movement speed
+    private float activeMoveSpeed;
+    [SerializeField] private float normalSpeed = 15.0f;
+    [SerializeField] private float dashSpeed = 30.0f;
+
+    // Rotation variables
     private float turnSmoothTime = 0.1f;
     private float turnSmoothVelocity;
 
-    // Dash
-    private float activeMoveSpeed;
-    [SerializeField]
-    private float dashSpeed = 30.0f;
-    [SerializeField]
-    private float dashLength = 0.5f;
-    [SerializeField]
-    private float dashCooldown = 1f;
-
+    // Dash Variables
+    [SerializeField] private float dashLength = 0.5f;
+    [SerializeField] private float dashCooldown = 1f;
+    // Internal Counters to manage Dash
     private float dashCounter;
     private float dashCooldownCounter;
 
-    // Interactions
-    public SpawnItems touching;
-
     private void Start()
     {
+        // Set Player speed to normal at the beginning
         activeMoveSpeed = normalSpeed;    
     }
 
     void Update()
     {
+        // Get the Input and create a vector based on it
         Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized;
 
+        // If move actually has input complete process to move Player object
         if (move.magnitude >= 0.1f)
         {
+            // Calculate Rotation of player and smoothen it for better visual
             float target_angle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target_angle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
+            // Move player according to prior input
             controller.Move(move * Time.deltaTime * activeMoveSpeed);
         }
 
-        if (Input.GetKeyDown(KeyCode.E) && touching.tag == "Item Spawner")
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            Debug.Log("Item Spawner");
-            // touching.SpawnProp();
-        }
-
-        if (Input.GetKeyDown(KeyCode.E) && touching.tag == "Counter")
-        {
-            Debug.Log("Counter");
+            Debug.Log("Pressed E");
         }
 
         if (Input.GetKeyDown(KeyCode.Escape)) //Escape key to open Main Menu
