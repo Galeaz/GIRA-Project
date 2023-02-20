440b66e6ad95a91d0e9f6fca058c09c15ccb9b43
Galeaz <50804911+Galeaz@users.noreply.github.com>
2023-02-19 22:22:54 -0800

    Added timer, finish canvas and audio
    timer has 90 sec, last 10 sec changes to red text and plays audio when time is 0 plus canvas enabled to show finish banner

---
+++ Art Rush/Assets/Scripts/CountDownTimer.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CountDownTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 90f; //time the level will have
+    bool timerToggle = true; //keeping track if timer enabled
+    
+    public Behaviour timeOutCanvas;
+    public AudioSource finishAudio;
+
+    [SerializeField] TextMeshProUGUI countTimeText; //time text object
+
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerToggle) //while time not 0 keeps counting
+        {
+            if (currentTime > 0) //while time != 0
+            {
+                currentTime -= Time.deltaTime; //countdown function
+            }
+            else //when time <= 0
+            {
+                timerToggle = false; //time run out
+                endGame();
+            }
+
+            if (currentTime <= 10) //when player has less than 10 seconds left
+            {
+                countTimeText.color = Color.red; //changing text color to red
+            }
+
+            DisplayTime(currentTime);
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        if (timeToDisplay < 0)
+        {
+            timeToDisplay = 0; //when the time runs out show 0
+        }
+        else if (timeToDisplay > 0)
+        {
+            timeToDisplay += 1; //this will make the time more accurate
+        }
+
+        //formating minutes and seconds for text
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        countTimeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    void endGame()
+    {
+        Time.timeScale = 0;
+        timeOutCanvas.enabled = true; //shows finish match banner
+        currentTime = 0; //stops showing negative time
+        finishAudio.Play(); //plays a small audio to indicate end of game
+
+        //ADD COUROUTINE FOR CHANGE OF SCREEN
+    }
+}
--- Art Rush/Assets/Scripts/MusicManager.cs
+++ Art Rush/Assets/Scripts/MusicManager.cs
@@ -2,21 +2,27 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
-    /* IT MADE THE MUSIC TOGGLE AND SLIDER USELESS
+
     //reference to itself
     private static MusicManager musicManagerInstance; //only one copy of this variable at the time so only 1 audio
+
+    void Start()
+    {
+        musicManagerInstance = GameObject.FindObjectOfType<MusicManager>(); //finding audio in hierarchy
+    }
 
     void Awake()
     {
         DontDestroyOnLoad(this); //this function will let the music sounds all the time not destroying
 
-        if(musicManagerInstance == null)
+        //keeping only 1 audio in game
+        if (musicManagerInstance == null) 
         {
-            musicManagerInstance = this;
+            musicManagerInstance = this; //no audio then create
         }
         else
         {
-            Destroy(gameObject);
+            Destroy(gameObject); //there is audio then destroy new one
         }
-    }*/
+    }
 }
