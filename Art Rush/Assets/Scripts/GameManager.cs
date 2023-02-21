33c6bb870995b43c763afec56dcebfc27587a8fd
Galeaz <50804911+Galeaz@users.noreply.github.com>
2023-02-20 16:04:12 -0800

    Added start banner
    Added the start banner at the beginning of gameplay, starts game paused after 3 secs canvas is disabled to play

--- Art Rush/Assets/Scripts/GameManager.cs
+++ Art Rush/Assets/Scripts/GameManager.cs
@@ -8,13 +8,29 @@ public class GameManager : MonoBehaviour
     public GameObject Pause_Button;
     public GameObject Resume_Button;
 
+    public Behaviour gameStartCanvas; //canvas start banner
 
+    void Start()
+    {
+        PauseGame(); //starts with game paused to avoid player from moving
+        StartCoroutine(ShowBannerAndContinue(3f)); //will disable start canvas banner after specified time
+    }
+
+    private IEnumerator ShowBannerAndContinue(float waitTime)
+    {
+        gameStartCanvas.enabled = true; //shows start match banner
+        yield return new WaitForSecondsRealtime(waitTime); //will wait to disable canvas banner
+
+        gameStartCanvas.enabled = false; //disables start match banner
+        ResumeGame(); //resume game to play
+    }
+    
     public void PauseGame()
     {
-        Time.timeScale = 0;
+        Time.timeScale = 0; //stops the game
     }
     public void ResumeGame()
     {
-        Time.timeScale = 1;
+        Time.timeScale = 1; //resumes game
     }
 }
