763f0dd2c5fe833be039a8f3f845bb835cac7bb2
reneett <reneetran2002@hotmail.com>
2023-03-19 17:18:19 -0700

    Network Menu Rework

--- Art Rush/Assets/Scripts/Networked/Launcher.cs
+++ Art Rush/Assets/Scripts/Networked/Launcher.cs
@@ -5,10 +5,13 @@ using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
 using System.Linq;
+using UnityEngine.SceneManagement;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
     public static Launcher Instance;
+    [SerializeField]
+    public GameObject Back_Button;
     #region Private Serilizable Fields
 
     [SerializeField]
@@ -153,6 +156,12 @@ public class Launcher : MonoBehaviourPunCallbacks
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
         Instantiate(playerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(newPlayer);
+    }
+
+    public void GoBack()
+    {
+        PhotonNetwork.Disconnect();
+        SceneManager.LoadScene("MainMenu");
     }
 
     #endregion
